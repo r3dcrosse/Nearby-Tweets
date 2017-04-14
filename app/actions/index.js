26f5fdd3da04a7c00fa664d0bba9ed64091d7a0c
r3dcrosse <mrdavidwayman@gmail.com>
2017-04-14 16:47:08 -0700

    cleans up comments

--- app/actions/index.js
+++ app/actions/index.js
@@ -5,20 +5,3 @@ export const GEOLOCATION_FETCH_FAILED = 'GEOLOCATION_FETCH_FAILED';
 export const requestGeoLocation = () => ({
   type: REQUEST_GEOLOCATION
 });
-
-// const getCurrentGeoLoc = () => dispatch => {
-//   dispatch(requestGeoLocation);
-//   dispatch(receiveGeoLocation({ longitude: 0, latitude: 0}));
-// }
-
-// const shouldFetchGeoLoc = state => {
-//   const geoLocation = state.geoLocation;
-//
-//   return geoLocation.isGettingCurrentGeoLoc ? false : true;
-// }
-//
-// export const fetchGeoLocIfNeeded = () => (dispatch, getState) => {
-//   if (shouldFetchGeoLoc(getState())) {
-//     return dispatch(requestGeoLocation);
-//   }
-// }
