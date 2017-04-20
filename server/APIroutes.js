2fc3b60c2c60092b1c646cd540df35ede7a68910
r3dcrosse <mrdavidwayman@gmail.com>
2017-04-19 22:12:17 -0700

    Adds response fake data for API route

--- server/APIroutes.js
+++ server/APIroutes.js
@@ -2,8 +2,16 @@ const APIrouter = require('express').Router();
 
 APIrouter.route('/')
   .get((request, response) => {
-    console.log('Trying to make a request to the twitter API');
-    response.status(200).send('Trying to make a request to the twiter API');
+    console.log('Trying to make a request to the twitter API with coords:');
+    console.log(request.query);
+
+    // TODO: error handling for no lat/lng in request.query
+    // if request.query.lat is undefined or lng is undefined
+
+    // else make the request to the twitter API
+    response.status(200).send({
+      tweets: ['fake', 'data', 'tweets']
+    });
   });
 
 APIrouter.route('*')
